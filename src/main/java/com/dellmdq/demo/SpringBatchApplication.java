5580dbea043cf94e2628774dce834b1e7441e212
dellerik <erik.dell@lamercantil.com.ar>
2022-11-04 16:54:33 -0300

    First step done

---
+++ src/main/java/com/dellmdq/demo/SpringBatchApplication.java
@@ -0,0 +1,17 @@
+package com.dellmdq.demo;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableBatchProcessing
+@ComponentScan("com.dellmdq.config")//de este paquete leeran los jobs y steps
+public class SpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchApplication.class, args);
+	}
+
+}
